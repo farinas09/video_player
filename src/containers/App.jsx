669657e69d70efd2973dbb180dc563b8d8a7f0c5
efarinas <efarinas@binarios.io>
2021-06-23 01:40:51 -0600

    add hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "../components/Header";
 import Search from "../components/Search";
 import Categories from "../components/Categories";
@@ -8,45 +8,56 @@ import Footer from "../components/Footer";
 
 import '../assets/styles/App.scss';
 
-const App = () => (
+const App = () => {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/initialState')
+            .then(res=> res.json())
+            .then(data => setVideos(data))
+    }, []);
+    console.log(videos);
+
+  return (
     <div className="App">
-        <Header />
-        <Search />
-        <Categories title='Mi lista'>
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Categories title='Tendencias'>
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Categories title='Originales'>
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Footer/>
+      <Header />
+      <Search />
+      <Categories title="Mi lista">
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      <Categories title="Tendencias">
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      <Categories title="Originales">
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      <Footer />
     </div>
-)
+  );
+};
 
 export default App;
