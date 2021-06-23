c4fe25be24aeaa04eeaedf3023a0b4be51e61d23
efarinas <efarinas@binarios.io>
2021-06-23 02:00:13 -0600

    create custom hook for api call

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,44 +1,44 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Header from "../components/Header";
 import Search from "../components/Search";
 import Categories from "../components/Categories";
 import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
 import Footer from "../components/Footer";
+import useInitialState from "../hooks/useInitialState";
 
 import "../assets/styles/App.scss";
 
-const App = () => {
-  const [videos, setVideos] = useState([]);
+const API = 'http://localhost:3000/initialState';
 
-  useEffect(() => {
-    fetch("http://localhost:3000/initialState")
-      .then((res) => res.json())
-      .then((data) => setVideos(data));
-  }, []);
+
+const App = () => {
+    const initialState = useInitialState(API)
 
   return (
     <div className="App">
       <Header />
       <Search />
-      {videos.myList?.length > 0 && 
+      {initialState.myList?.length > 0 && 
         <Categories title="Mi lista">
           <Carousel>
-            <CarouselItem />
+          { initialState.myList?.map(item =>
+                    <CarouselItem key={item.id} {...item}/>
+            )}
           </Carousel>
         </Categories>
       }
 
       <Categories title="Tendencias">
         <Carousel>
-            { videos.trends?.map(item =>
+            { initialState.trends?.map(item =>
                     <CarouselItem key={item.id} {...item}/>
             )}
         </Carousel>
       </Categories>
       <Categories title="Originals">
         <Carousel>
-            { videos.originals?.map(item =>
+            { initialState.originals?.map(item =>
                     <CarouselItem key={item.id} {...item}/>
             )}
         </Carousel>
