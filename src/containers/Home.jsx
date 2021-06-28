37b4ff59899c407b3d532d29303c468df04dfeed
efarinas <efarinas@binarios.io>
2021-06-28 01:04:33 -0600

    add router

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -12,7 +12,7 @@ import "../assets/styles/App.scss";
 const API = 'http://localhost:3000/initialState';
 
 
-const App = () => {
+const Home = () => {
     const initialState = useInitialState(API)
 
   return (
@@ -48,4 +48,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Home;
