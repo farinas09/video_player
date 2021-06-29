02eb0f9f6e3e180946e745fd6d976c1c6f311ea0
efarinas <efarinas@binarios.io>
2021-06-29 11:19:27 -0600

    add redux events

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../assets/styles/components/Header.scss";
 import logo from "../assets/static/logo-platzi-video-BW2.png";
 import userIcon from "../assets/static/user-icon.png";
 
 const Header = () => (
   <header className="header">
-    <img className="header__img" src={logo} alt="Video Player" />
+    <Link to="/">
+      <img className="header__img" src={logo} alt="Video Player" />
+    </Link>
     <div className="header__menu">
       <div className="header__menu--profile">
         <img src={userIcon} alt="" />
@@ -13,10 +16,10 @@ const Header = () => (
       </div>
       <ul>
         <li>
-          <a href="/">Cuenta</a>
+          <Link to="/">Cuenta</Link>
         </li>
         <li>
-          <a href="/">Cerrar Sesión</a>
+          <Link to="/login">Iniciar Sesión</Link>
         </li>
       </ul>
     </div>
