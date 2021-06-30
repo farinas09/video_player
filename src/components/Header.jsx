8ede14b1932d91bf12a495003021ae1e2763a374
efarinas <efarinas@binarios.io>
2021-06-29 19:00:42 -0600

    create fake login and implement gravatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,67 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { logoutRequest } from "../actions";
+import gravatar from "../utils/gravatar";
 import "../assets/styles/components/Header.scss";
 import logo from "../assets/static/logo-platzi-video-BW2.png";
 import userIcon from "../assets/static/user-icon.png";
 
-const Header = () => (
-  <header className="header">
-    <Link to="/">
-      <img className="header__img" src={logo} alt="Video Player" />
-    </Link>
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={userIcon} alt="" />
-        <p>Perfil</p>
-      </div>
-      <ul>
-        <li>
-          <Link to="/">Cuenta</Link>
-        </li>
-        <li>
-          <Link to="/login">Iniciar Sesión</Link>
-        </li>
-      </ul>
-    </div>
-  </header>
-);
+const Header = (props) => {
+  const { user } = props;
+  const hasUser = Object.keys(user).length > 0;
 
-export default Header;
+  const handleLogout = () => {
+    props.logoutRequest({});
+  };
+
+  return (
+    <header className="header">
+      <Link to="/">
+        <img className="header__img" src={logo} alt="Video Player" />
+      </Link>
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          {hasUser ? (
+            <img src={gravatar(user.email)} alt="user.email" />
+          ) : (
+            <img src={userIcon} alt="" />
+          )}
+
+          <p>Perfil</p>
+        </div>
+        <ul>
+          {hasUser ? (
+            <li>
+              <Link to="/">{user.name}</Link>
+            </li>
+          ) : null}
+
+          {hasUser ? (
+            <li>
+              <Link to="/" onClick={handleLogout}>
+                Logout
+              </Link>
+            </li>
+          ) : (
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          )}
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,6 +12,16 @@ const reducer = (state, action) => {
         ...state,
         myList: state.myList.filter((items) => items.id !== action.payload),
       };
+    case "LOGIN_REQUEST":
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case "LOGOUT_REQUEST":
+      return {
+        ...state,
+        user: {},
+      };
     default:
       return state;
   }
